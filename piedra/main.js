ad5d662d37ec64544a7980fe7b209570832cd1dd
angeloLuna <angel.luna.najera@gmail.com>
2018-10-19 20:27:11 -0500

    flexbox

--- piedra/main.js
+++ piedra/main.js
@@ -1,6 +1,7 @@
 let j1 = prompt('elige');
 let j2 = prompt('elige también');
 
+// Resolución utilizando anidación de condiciones
 if(j1 === 'piedra'){
   if(j2 === 'piedra'){
     console.log('empate');
@@ -15,7 +16,7 @@ if(j1 === 'piedra'){
   }else if( j2 === 'papel'){
     console.log('Empate');
   }else{
-    console.log('gana j2')
+    console.log('gana j2');
   }
 }else if( j1 === 'tijera'){
   if(j2 === 'piedra'){
@@ -27,4 +28,21 @@ if(j1 === 'piedra'){
   }
 }else{
   console.log('debes elegir entre piedra papel o tijera');
+}
+
+// Resolución utilizando operadores lógicos
+if( j1 === 'piedra' && j2 === 'piedra'){
+  console.log('Empate')
+}else if( j1 === 'papel' && j2 === 'papel'){
+  console.log('Empate')
+}else if( j1 === 'tijera' && j2 === 'tijera'){
+  console.log('Empate')
+}
+
+if(j1 === 'piedra' && j2 === 'tijera'){
+  console.log('Gana jugador 1')
+}else if(j1 === 'papel' && j2 === 'piedra'){
+  console.log('Gana jugador 1')
+}else if(j1 === 'tijera' && j2 === 'papel'){
+  console.log('Gana jugador 1')
 }
